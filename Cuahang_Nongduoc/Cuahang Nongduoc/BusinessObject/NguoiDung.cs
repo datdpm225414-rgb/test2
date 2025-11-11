90246cdd8ae864a4f6ec020325f65846c8109d35
Trần Nam Đạt <dat_dpm225414@student.agu.edu.vn>
2025-11-11 14:37:46 +0700

    abcv

---
+++ Cuahang_Nongduoc/Cuahang Nongduoc/BusinessObject/NguoiDung.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuahangNongduoc
+{
+    public class NguoiDung
+    {
+        public string UserID { get; set; }
+        public string TenDangNhap { get; set; }
+        public string MatKhauHash { get; set; }
+        public string VaiTro { get; set; }
+        public string TenNguoiDung { get; set; }
+        public string Email { get; set; }
+        public string SoDienThoai { get; set; }
+        public bool TrangThai { get; set; }
+    }
+}
---
+++ Cuahang_Nongduoc/Cuahang Nongduoc/frmDanhsachPhieuNhap.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using CuahangNongduoc.Controller;
+using CuahangNongduoc.Strategy;
+
+namespace CuahangNongduoc
+{
+    public partial class frmDanhsachPhieuNhap : Form
+    {
+        public frmDanhsachPhieuNhap()
+        {
+            InitializeComponent();
+        }
+
+        PhieuNhapController ctrl = new PhieuNhapController();
+        NhaCungCapController ctrlNCC = new NhaCungCapController();
+        NguoiDungController ctrlND = new NguoiDungController(); // Them
+        //Co sua
+
+        private void frmDanhsachPhieuNhap_Load(object sender, EventArgs e)
+        {
+            dataGridView.AutoGenerateColumns = false;
+            ctrlNCC.HienthiDataGridviewComboBox(colNhaCungCap);
+            ctrlND.HienthiNguoiDungDataGridviewComboBox(colNguoiDung); //Người lập
+            ctrl.HienthiPhieuNhap(bindingNavigator, dataGridView);
+        }
+        frmNhapHang NhapHang = null;
+        private void dataGridView_DoubleClick(object sender, EventArgs e)
+        {
+            if (NhapHang == null || NhapHang.IsDisposed)
+            {
+                NhapHang = new frmNhapHang(ctrl);
+                NhapHang.Show();
+            }
+            else
+                NhapHang.Activate();
+        }
+
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+            if (NhapHang == null || NhapHang.IsDisposed)
+            {
+                NhapHang = new frmNhapHang();
+                NhapHang.Show();
+            }
+            else
+                NhapHang.Activate();
+        }
+
+        private void toolIn_Click(object sender, EventArgs e)
+        {
+            DataRowView row = (DataRowView)bindingNavigator.BindingSource.Current;
+            if (row != null)
+            {
+                PhieuNhapController ctrlPN = new PhieuNhapController();
+                String ma_phieu = row["ID"].ToString();
+                CuahangNongduoc.BusinessObject.PhieuNhap ph =  ctrlPN.LayPhieuNhap(ma_phieu);
+                frmInPhieuNhap PhieuNhap = new frmInPhieuNhap(ph);
+                PhieuNhap.Show();
+            }
+        }
+        //Co sua
+
+        private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có chắc chắn xóa không?", "Phieu Nhap", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                if (dataGridView.SelectedRows.Count > 0)
+                {
+                    var policy = new XoaMem();
+
+                    DataGridViewRow row = dataGridView.SelectedRows[0];
+                    string id = row.Cells["colId"].Value.ToString();
+                    if (ThamSo.Delete(id, "PHIEU_NHAP", policy))
+                    {
+                        MessageBox.Show("Xóa thành công!", "Phieu Ban Nhap", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        frmDanhsachPhieuNhap_Load(sender, e);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Xóa thất bại!");
+                    }
+                    //bindingNavigator.BindingSource.RemoveCurrent();
+
+                }//ctrl.Save();
+                }
+        }
+
+        private void toolTimKiem_Click(object sender, EventArgs e)
+        {
+            
+            frmTimPhieuNhap TimPhieu = new frmTimPhieuNhap();
+            Point p = PointToScreen(toolTimKiem.Bounds.Location);
+            p.X += toolTimKiem.Width;
+            p.Y += toolTimKiem.Height;
+            TimPhieu.Location = p;
+            TimPhieu.ShowDialog();
+            if (TimPhieu.DialogResult == DialogResult.OK)
+            {
+                ctrl.TimPhieuNhap(TimPhieu.cmbNCC.SelectedValue.ToString(), TimPhieu.dtNgayNhap.Value.Date);
+            }
+        }
+    }
+}
